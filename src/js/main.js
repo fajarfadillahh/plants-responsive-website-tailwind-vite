f73820c351994b98d90dbee478f769e1c808fb0a
fajarfadillahh <fajaragustian979@gmail.com>
2022-05-19 21:46:26 +0700

    header section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
 import "../css/index.css";
+
+// ===== SHOW MENU =====
+function showMenu(menuId, toggleId, closeId) {
+  const menu = document.getElementById(menuId);
+  const toggle = document.getElementById(toggleId);
+  const close = document.getElementById(closeId);
+
+  if (menu && toggle && close) {
+    toggle.onclick = () => {
+      menu.classList.add("show-menu");
+    };
+    close.onclick = () => {
+      menu.classList.remove("show-menu");
+    };
+  }
+}
+showMenu("header-menu", "header-toggle", "header-close");
