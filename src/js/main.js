1e208deccfa4068562dfb2735bfaf2bdabfb336c
fajarfadillahh <fajaragustian979@gmail.com>
2022-05-24 20:48:58 +0700

    questions section

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,31 @@ function showMenu(menuId, toggleId, closeId) {
   }
 }
 showMenu("header-menu", "header-toggle", "header-close");
+
+// ===== ACCORDION QUESTIONS SECTION =====
+const accordionItems = document.querySelectorAll(".questions__accordion-item");
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".questions__accordion-header");
+
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
+
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".questions__accordion-content");
+
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};
